e74a30031acd2cbf28440d9ee65122ee39b8225c
erupturatis <bb.octav@gmail.com>
2022-03-20 21:02:42 +0200

    need to reverse engineer api

---
+++ Main.py
@@ -0,0 +1,12 @@
+import fetching
+
+def calculate_correlation()->int:
+    pass
+
+
+def main():
+    # does correlation for fixed variables
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ reading.py
@@ -0,0 +1,4 @@
+
+
+def read_input():
+    pass
