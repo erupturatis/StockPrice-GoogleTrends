5230c9b778d94d6f9e38e204ad94f0cc1329153a
erupturatis <bb.octav@gmail.com>
2022-03-30 21:27:37 +0300

    misc

--- scripts/webscrapping/google_trends.py
+++ scripts/webscrapping/google_trends.py
@@ -14,59 +14,105 @@ class GoogleTrendsRequest(object):
     HL = "en-US"
     TZ = -120
 
-    def __init__(self, keyword) -> None:
-        self.keyword = keyword
-
-    def request_trends(self):
-
-        return self
+    def __init__(self) -> None:
+        pass   
 
 
     def get_cookie(self):
-        """
+        '''
         Gets a google cookie in order to not get error 429 too many requests
-        """
-        cookie_req = self.server.get(url=self.GEO_URL)
+        '''
+        cookie_req = self.session.get(url=self.GEO_URL)
+        self.headers = cookie_req.request.headers
 
-    def init_request(self):
-        server = requests.session()
-        self.server = server
+    def init_requester(self):
+        session = requests.session()
+        self.session = session
 
-    def get_request(self, url, **params):
+    def verification_needed(func):
+        #decorator to call verify_initialization on all functions where is needed 
+        def function_wrapper(*args):
+            self = args[0]
+            self.verify_initialization()
+            func(self)
+        return function_wrapper
+
+
+    def get_request(self, url, **payload):
+        # gets the actual payload
+        payload = payload['payload']
+        # makes the request and returns the object
+        get_req = self.session.get(url=url, params = payload, headers = self.headers)
+        return get_req
+
+
+    def get_trend(self, days = 90) -> list:
         pass
+    
 
-    def get_trends(self):
-        pass
-
-
-
-    def test_request(self):
-        self.get_cookie()
+    def get_tokens(self, keyword = 'ukraine') -> None:
+        # get the tokes needed to create the request for trend over time
         payload = {
             'hl':'en-US',
             'tz':'-180',
-            'req':{'comparisonItem':[{'keyword':'ukraine','geo':'','time':'today 12-m'}],'category':0,'property':''},
+            'req':{'comparisonItem':[{f'{keyword}':'ukraine','geo':'','time':'today 3-m'}],'category':0,'property':''},
         }
-
-        element = open("textfile.txt","w",encoding="utf-8")
-
         payload['req'] = json.dumps(payload['req'])
-        url = 'https://trends.google.com/trends/api/explore'
 
-        try:
-            get_request = self.server.get(url, params = payload )
-        except:
-            print("request is not initialized")
+        get_req = self.get_request(self.STANDARD_URL, payload = payload)
+        print(get_req.text['widgets'])
 
+
+    def verify_initialization(self):
+        # if a session is not created yet, it will make one 
+        if hasattr(self, 'session'):
+            print("session already created")
+        else:
+            self.init_requester()
+            print("creating session ")
+            
+
+    @verification_needed
+    def get_trend_flow(self, keyword = 'ukraine', days = 90) -> None:
+        if not hasattr(self, 'headers'):
+            self.get_cookie()
+
+        self.get_tokens(keyword=keyword)
+
+        response = self.get_trend(days=days)
+            
+        
+
+    
+
+    def test_stuff(self):
+
+        self.init_requester()
+        self.get_cookie()
+
+        payload = {
+            'hl':'en-US',
+            'tz':'-180',
+            'req':{'comparisonItem':[{f'{keyword}':'ukraine','geo':'','time':'today 3-m'}],'category':0,'property':''},
+        }
+        payload['req'] = json.dumps(payload['req'])
+
+        element = open("textfile.txt",'w')
+
+        get_req = self.get_request(self.STANDARD_URL, payload = payload)
+        print(get_req.text['widgets'])
 
         #print(get_request.request.headers)
-        element.write(get_request.text) 
+        element.write(get_req.text) 
         element.close()
 
 
         return
 
 
+    
+   
+
 trends_requester = GoogleTrendsRequest()
-trends_requester.init_request()
-trends_requester.test_request()
+#trends_requester.test2 = trends_requester.decorator(trends_requester.test2)
+trends_requester.test2()
